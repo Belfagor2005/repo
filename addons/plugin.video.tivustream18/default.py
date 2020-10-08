3d1f4ca8cea73ca4bfcdb7b8f56a7cd22173c98d
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2020-10-08 21:08:43 +0200

    Up

--- addons/plugin.video.tivustream18/default.py
+++ addons/plugin.video.tivustream18/default.py
@@ -1231,7 +1231,7 @@ def showContent11(name, url):
                         print("name =", name)
                         print("url =", url)
                         url = "http" + url
-                        url = url.replace("%3a",":")
+                        url = url.replace("%2F","/").replace("%3A",":").replace("%3a",":").replace("%0D%0A","").strip('\r\n')
                         print("url 2=", url)
                         pic = i_xxx
                         # addDirectoryItem(name, url,23, pic, fanart)
