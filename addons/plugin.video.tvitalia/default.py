fc6e7e68cf83f5f97db8b93cc1a7d73f12b4f5e9
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2020-12-26 11:03:51 +0100

    Up Paesi Int.

--- addons/plugin.video.tvitalia/default.py
+++ addons/plugin.video.tvitalia/default.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: latin-1 -*-
-import sys, xpath, xbmc,os 
+import sys, xpath, xbmc,os
 libs = sys.argv[0].replace("default.py", "resources/lib")
 if os.path.exists(libs):
    sys.path.append(libs)
@@ -9,7 +9,7 @@ if ("?plugin%3A%2F%2F" in sys.argv[2]) or ("?plugin://" in sys.argv[2]):
        argtwo = sys.argv[2]
        n2 = argtwo.find("?", 0)
        n3 = argtwo.find("?", (n2+2))
-       if n3<0: 
+       if n3<0:
               sys.argv[0] = argtwo
               sys.argv[2] = ""
        else:
@@ -18,7 +18,7 @@ if ("?plugin%3A%2F%2F" in sys.argv[2]) or ("?plugin://" in sys.argv[2]):
        sys.argv[0] = sys.argv[0].replace("?", "")
 
 else:
-       sys.argv[0] = sys.argv[0].replace('/usr/lib/enigma2/python/Plugins/Extensions/KodiLite/plugins/', 'plugin://') 
+       sys.argv[0] = sys.argv[0].replace('/usr/lib/enigma2/python/Plugins/Extensions/KodiLite/plugins/', 'plugin://')
        sys.argv[0] = sys.argv[0].replace('default.py', '')
 print "dclip Here in default-py sys.argv B=", sys.argv
 
@@ -79,18 +79,22 @@ def getUrl2(url, referer):
 	response = urllib2.urlopen(req)
 	link=response.read()
 	response.close()
-	return link	
+	return link
 
 def showContent():
                 names = []
                 urls = []
                 modes = []
+
+                names.append("Paesi")
+                urls.append("https://www.tvdream.net/web-tv/paesi/")
+                modes.append("41")
+                names.append("Italia")
+                urls.append("https://www.tvdream.net/web-tv/paesi/italia/")
+                modes.append("21")
                 names.append("Regioni")
                 urls.append("https://www.tvdream.net/web-tv/regioni/")
                 modes.append("11")
-                names.append("Paesi")
-                urls.append("https://www.tvdream.net/web-tv/paesi/italia/")
-                modes.append("21")
                 names.append("VOD")
                 urls.append("https://www.raiplay.it/")
                 modes.append("31")
@@ -102,25 +106,45 @@ def showContent():
                         i = i+1
                         addDirectoryItem(name, {"name":name, "url":url, "mode":mode}, pic)
                 xbmcplugin.endOfDirectory(thisPlugin)
-                
+
 ##########################################################
-def showContent11(name, urlmain):
+def showContent10(name, urlmain):
         content = getUrl(urlmain)
 #        print "content A =", content
         icount = 0
 	start = 0
-	n1 = content.find('ul class="menu-sub"', start)
+	n1 = content.find('<ul class="menu-sub">"', start)
 	if n1<0:
                 return
-        n2 = content.find("</ul>", n1)     
+        n2 = content.find("</ul>", n1)
         content = content[n1:n2]
-        print "content A2 =", content     
+        print "content A2 =", content
+        pic = " "
+        regexcat = 'href="(.*?)">(.*?)<'
+        match = re.compile(regexcat,re.DOTALL).findall(content)
+        for url, name in match:
+                addDirectoryItem(name, {"name":name, "url":url, "mode":11}, pic)
+        xbmcplugin.endOfDirectory(thisPlugin)
+
+
+def showContent11(name, urlmain):
+        content = getUrl(urlmain)
+#        print "content A =", content  #paesi
+        icount = 0
+	start = 0
+	n1 = content.find('menu-sub">', start)
+	if n1<0:
+                return
+        n2 = content.find("</ul>", n1)
+        content = content[n1:n2]
+        print "content A2 =", content
         pic = " "
         regexcat = 'href="(.*?)">(.*?)<'
         match = re.compile(regexcat,re.DOTALL).findall(content)
         for url, name in match:
                 addDirectoryItem(name, {"name":name, "url":url, "mode":12}, pic)
         xbmcplugin.endOfDirectory(thisPlugin)
+
 
 def showContent12(name, url):
                 pages = [1, 2]
@@ -130,7 +154,7 @@ def showContent12(name, url):
                         pic = " "
                         addDirectoryItem(name, {"name":name, "url":url1, "mode":13}, pic)
                 xbmcplugin.endOfDirectory(thisPlugin)
-                
+
 def showContent13(name1, urlmain):
 	content = getUrl(urlmain)
 	print " content B =", content
@@ -142,7 +166,7 @@ def showContent13(name1, urlmain):
                  pic = " "
 	         addDirectoryItem(name, {"name":name, "url":url, "mode":14}, pic)
         xbmcplugin.endOfDirectory(thisPlugin)
-        
+
 def playVideo14(name, urlmain):
 	content = getUrl(urlmain)
 	print " content B =", content
@@ -162,71 +186,73 @@ def playVideo14(name, urlmain):
         li = xbmcgui.ListItem(name,iconImage="DefaultFolder.png", thumbnailImage=pic)
         player = xbmc.Player()
         player.play(url2, li)
-##########################################################        
-        
+##########################################################
+
 def showContent21(name, url):
                 np = 28
                 page = 1
                 while page < np:
                         url1 = url + "page/" + str(page) + "/"
-                        name = "Italia-Page " + str(page)
+                        name = "Page " + str(page)
                         pic = " "
                         page = page+1
                         addDirectoryItem(name, {"name":name, "url":url1, "mode":22}, pic)
-                xbmcplugin.endOfDirectory(thisPlugin)   
-                
+                xbmcplugin.endOfDirectory(thisPlugin)
+
+
+
 def showContent22(name, url):
         content = getUrl(url)
         print "showContent2 content =", content
-  
+
         pic = " "
         regexcat = '<div class="item-head.*?a href="(.*?)".*?bookmark">(.*?)<'
         match = re.compile(regexcat,re.DOTALL).findall(content)
         print "showContent2 match =", match
-        
+
         for url, name in match:
                 if ("rai" in url.lower()) or ("rai" in name.lower()):
                        mode = 23
                 else:
                        mode = 24
                 addDirectoryItem(name, {"name":name, "url":url, "mode":mode}, pic)
-        xbmcplugin.endOfDirectory(thisPlugin)                
-                
+        xbmcplugin.endOfDirectory(thisPlugin)
+
 def showContent23(name, urlmain):
 	content = getUrl(urlmain)
         print "getVideos content =", content
-  
+
         pic = " "
         regexcat = '"player".*?href="(.*?)"'
         match = re.compile(regexcat,re.DOTALL).findall(content)
         print "getVideos match =", match
-        
+
         url2 = match[0]
         content2 = getUrl(url2)
         print "getVideos content2 =", content2
         regexcat2 = 'liveVideo":{"mediaUrl":"(.*?)"'
         match2 = re.compile(regexcat2,re.DOTALL).findall(content2)
-        print "getVideos match2 =", match2        
-        url3 = match2[0]        
+        print "getVideos match2 =", match2
+        url3 = match2[0]
         play(name, url3)
-        
+
 def showContent24(name, urlmain):
 	content = getUrl(urlmain)
         print "getVideos2 content =", content
-        
+
         regexcat = '"player".*?href="(.*?)"'
         match = re.compile(regexcat,re.DOTALL).findall(content)
         print "getVideos2 match =", match
-        
+
         url2 = match[0]
         content2 = getUrl(url2)
         print "getVideos2 content2 =", content2
-        
+
         n1 = content2.find(".m3u8")
         n2 = content2.rfind("http", 0, n1)
         url3 = content2[n2:(n1+5)]
         print "getVideos2 url3 =", url3
-        
+
         play(name, url3)
 
 def play(name, urlmain):
@@ -235,8 +261,117 @@ def play(name, urlmain):
         li = xbmcgui.ListItem(name,iconImage="DefaultFolder.png", thumbnailImage=pic)
         player = xbmc.Player()
         player.play(urlmain, li)
-                        
-                     	                      
+
+def showContent41(name, url):
+                names = []
+                urls = []
+                modes = []
+                names.append("Albania")
+                urls.append("https://www.tvdream.net/web-tv/paesi/albania/")
+                modes.append("21")
+                names.append("Argentina")
+                urls.append("https://www.tvdream.net/web-tv/paesi/argentina/")
+                modes.append("21")
+                names.append("Bangladesh")
+                urls.append("https://www.tvdream.net/web-tv/paesi/bangladesh/")
+                modes.append("21")
+                names.append("Bulgaria")
+                urls.append("https://www.tvdream.net/web-tv/paesi/bulgaria/")
+                modes.append("21")
+                names.append("Canada")
+                urls.append("https://www.tvdream.net/web-tv/paesi/canada/")
+                modes.append("21")
+                names.append("Cina")
+                urls.append("https://www.tvdream.net/web-tv/paesi/cina/")
+                modes.append("21")
+                names.append("Corea del Sud")
+                urls.append("https://www.tvdream.net/web-tv/paesi/corea-del-sud/")
+                modes.append("21")
+                names.append("Croazia")
+                urls.append("https://www.tvdream.net/web-tv/paesi/croazia/")
+                modes.append("21")
+                names.append("Francia")
+                urls.append("https://www.tvdream.net/web-tv/paesi/francia/")
+                modes.append("21")
+                names.append("Germania")
+                urls.append("https://www.tvdream.net/web-tv/paesi/germania/")
+                modes.append("21")
+                names.append("Giappone")
+                urls.append("https://www.tvdream.net/web-tv/paesi/giappone/")
+                modes.append("21")
+                names.append("Grecia")
+                urls.append("https://www.tvdream.net/web-tv/paesi/grecia/")
+                modes.append("21")
+                names.append("Italia")
+                urls.append("https://www.tvdream.net/web-tv/paesi/italia/")
+                modes.append("21")
+                names.append("Malta")
+                urls.append("https://www.tvdream.net/web-tv/paesi/malta/")
+                modes.append("21")
+                names.append("Nigeria")
+                urls.append("https://www.tvdream.net/web-tv/paesi/nigeria/")
+                modes.append("21")
+                names.append("Olanda")
+                urls.append("https://www.tvdream.net/web-tv/paesi/olanda/")
+                modes.append("21")
+                names.append("Polonia")
+                urls.append("https://www.tvdream.net/web-tv/paesi/polonia/")
+                modes.append("21")
+                names.append("Portogallo")
+                urls.append("https://www.tvdream.net/web-tv/paesi/portogallo/")
+                modes.append("21")
+                names.append("Qatar")
+                urls.append("https://www.tvdream.net/web-tv/paesi/qatar/")
+                modes.append("21")
+                names.append("Regno Unito")
+                urls.append("https://www.tvdream.net/web-tv/paesi/regno-unito/")
+                modes.append("21")
+                names.append("Romania")
+                urls.append("https://www.tvdream.net/web-tv/paesi/romania/")
+                modes.append("21")
+                names.append("Russia")
+                urls.append("https://www.tvdream.net/web-tv/paesi/russia/")
+                modes.append("21")
+                names.append("Slovacchia")
+                urls.append("https://www.tvdream.net/web-tv/paesi/slovacchia/")
+                modes.append("21")
+                names.append("Spagna")
+                urls.append("https://www.tvdream.net/web-tv/paesi/spagna/")
+                modes.append("21")
+                names.append("Svezia")
+                urls.append("https://www.tvdream.net/web-tv/paesi/svezia/")
+                modes.append("21")
+                names.append("Svizzera")
+                urls.append("https://www.tvdream.net/web-tv/paesi/svizzera/")
+                modes.append("21")
+                names.append("Turchia")
+                urls.append("https://www.tvdream.net/web-tv/paesi/turchia/")
+                modes.append("21")
+                names.append("Ucraina")
+                urls.append("https://www.tvdream.net/web-tv/paesi/ucraina/")
+                modes.append("21")
+                names.append("Ungheria")
+                urls.append("https://www.tvdream.net/web-tv/paesi/ungheria/")
+                modes.append("21")
+                names.append("USA")
+                urls.append("https://www.tvdream.net/web-tv/paesi/usa/")
+                modes.append("21")
+                names.append("Venezuela")
+                urls.append("https://www.tvdream.net/web-tv/paesi/venezuela/")
+                modes.append("21")
+                pic = " "
+                i = 0
+                for name in names:
+                        url = urls[i]
+                        mode = modes[i]
+                        i = i+1
+                        addDirectoryItem(name, {"name":name, "url":url, "mode":mode}, pic)
+                xbmcplugin.endOfDirectory(thisPlugin)
+
+
+
+
+
 ##########################################################
 def showContent31(name, url):
                 names = []
@@ -257,7 +392,7 @@ def showContent31(name, url):
                         i = i+1
                         addDirectoryItem(name, {"name":name, "url":url, "mode":mode}, pic)
                 xbmcplugin.endOfDirectory(thisPlugin)
-                
+
 def showContent32(name, url):
         content = getUrl(url)
         print "showContent2 content =", content
@@ -270,7 +405,7 @@ def showContent32(name, url):
                 pic = " "
                 addDirectoryItem(name, {"name":name, "url":url, "mode":33}, pic)
         xbmcplugin.endOfDirectory(thisPlugin)
-        
+
 
 def showContent33(name, url):
         print "showContent3 name =", name
@@ -283,10 +418,10 @@ def showContent33(name, url):
         n2 = content.find("card-list__title", n1)
         if n2 == -1:
                 n2 = len(content)
-        print "showContent3 n1, n2 =", n1, n2        
-        content2 = content[n1:n2]        
+        print "showContent3 n1, n2 =", n1, n2
+        content2 = content[n1:n2]
         print "showContent3 content2 =", content2
-  
+
         pic = " "
         regexcat = '<a aria-label=.*?href="(.*?)"'
         match = re.compile(regexcat,re.DOTALL).findall(content2)
@@ -299,16 +434,16 @@ def showContent33(name, url):
                         name = url
                         url1 = "https://www.raiplay.it" + url
                         addDirectoryItem(name, {"name":name, "url":url1, "mode":34}, pic)
-        else:                
+        else:
                 for url in match:
                         name = url
                         url1 = "https://www.raiplay.it" + url
                         addDirectoryItem(name, {"name":name, "url":url1, "mode":34}, pic)
-                        
+
         xbmcplugin.endOfDirectory(thisPlugin)
-                
+
 def playVideo34(name, url):
-           print "In playVideo2 url =", url  
+           print "In playVideo2 url =", url
            cmd = "python '/usr/lib/enigma2/python/Plugins/Extensions/KodiLite/__main__.py' --no-check-certificate --skip-download -f best --get-url '" + url + "' > " + vid # /tmp/vid.txt"
            print "In playVideo2 cmd =", cmd
            if os.path.exists(vid):
@@ -322,9 +457,9 @@ def playVideo34(name, url):
            if not os.path.exists(vidurl):
               os.system("sleep 5")
               play(name, vidurl)
-           else: 
-              play(name, vidurl)           
-#######################           
+           else:
+              play(name, vidurl)
+#######################
 def showContent321(name, url):
         names = []
         urls = []
@@ -361,14 +496,14 @@ def showContent321(name, url):
                 addDirectoryItem(name, {"name":name, "url":url1, "mode":34}, pic)
         xbmcplugin.endOfDirectory(thisPlugin)
 
-        
+
 
 std_headers = {
 	'User-Agent': 'Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.2.6) Gecko/20100627 Firefox/3.6.6',
 	'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.7',
 	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
 	'Accept-Language': 'en-us,en;q=0.5',
-}  
+}
 
 def addDirectoryItem(name, parameters={},pic=""):
     li = xbmcgui.ListItem(name,iconImage="DefaultFolder.png", thumbnailImage=pic)
@@ -396,37 +531,43 @@ mode =  str(params.get("mode", ""))
 if not sys.argv[2]:
 	ok = showContent()
 else:
+
+
         if mode == str(11):
                 ok = showContent11(name, url)
+
+        elif mode == str(10):
+                ok = showContent10(name, url)
         elif mode == str(12):
-                ok = showContent12(name, url)        
+                ok = showContent12(name, url)
         elif mode == str(13):
-                ok = showContent13(name, url)         
+                ok = showContent13(name, url)
         elif mode == str(14):
                 ok = playVideo14(name, url)
-                
+
         elif mode == str(21):
-                ok = showContent21(name, url)        
+                ok = showContent21(name, url)
         elif mode == str(22):
-                ok = showContent22(name, url)              
+                ok = showContent22(name, url)
         elif mode == str(23):
-                ok = showContent23(name, url)  
+                ok = showContent23(name, url)
         elif mode == str(24):
-                ok = showContent24(name, url) 
-                
+                ok = showContent24(name, url)
+
         elif mode == str(31):
-                ok = showContent31(name, url)   
+                ok = showContent31(name, url)
         elif mode == str(32):
-                ok = showContent32(name, url)   
-                
+                ok = showContent32(name, url)
+
         elif mode == str(321):
-                ok = showContent321(name, url)        
+                ok = showContent321(name, url)
         elif mode == str(33):
                 ok = showContent33(name, url)
-	elif mode == str(34):        
-		ok = playVideo34(name, url)	
+	elif mode == str(34):
+		ok = playVideo34(name, url)
 
-
+	elif mode == str(41):
+		ok = showContent41(name, url)
 
 
 
